9a47d8fbb5a09aad9390de69925477531c82e44f
VictorVidal08 <victorvidal08@hotmail.com>
2022-12-06 19:55:19 -0300

    feat: adding user email, password and userName middleware validation

--- src/routes/userRoute.ts
+++ src/routes/userRoute.ts
@@ -1,10 +1,11 @@
 const { Router } = require('express');
 
 const userController = require('../controllers/userController');
+const { userValidation } = require('../middlewares/userValidation');
 
 const userRouter = Router();
 
 userRouter.get('/users', userController.findAll);
-userRouter.post('/user', userController.create);
+userRouter.post('/user', userValidation, userController.create);
 
 export default userRouter;
